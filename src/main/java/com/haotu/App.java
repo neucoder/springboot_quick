bb2bf86c42ca54d276442ba47ac14857de18ee97
ys <neucoder@163.com>
2023-09-03 22:44:23 +0800

    spring ctx bean define

--- src/main/java/com/haotu/App.java
+++ src/main/java/com/haotu/App.java
@@ -2,12 +2,16 @@ package com.haotu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(App.class, args);
+		for (String beanDefinitionName : ctx.getBeanFactory().getBeanDefinitionNames()) {
+			System.out.println(beanDefinitionName);
+		}
 	}
 
 }
