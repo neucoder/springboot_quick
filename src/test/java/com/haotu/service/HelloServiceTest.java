71fb096571d1eb30402a414e0f33e703d90438d3
ys <neucoder@163.com>
2023-11-04 16:27:12 +0800

    日志

--- src/test/java/com/haotu/service/HelloServiceTest.java
+++ src/test/java/com/haotu/service/HelloServiceTest.java
@@ -2,15 +2,22 @@ package com.haotu.service;
 
 import com.haotu.BaseTestNg;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
 
 public class HelloServiceTest extends BaseTestNg {
+
+    Logger logger =  LoggerFactory.getLogger(HelloServiceTest.class);
+
     @Resource
     private HelloService helloService;
 
     @Test
     public void testHello() {
-        System.out.println(helloService.hello());
+        logger.info("{}-{}", "ad", 11);
+        logger.error("this is a error");
+        logger.warn("this is a warn");
     }
 }
