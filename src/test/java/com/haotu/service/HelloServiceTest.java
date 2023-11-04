f6dc90e92266c6f885c1a2e41d45edabdf7de03a
ys <neucoder@163.com>
2023-11-04 16:38:37 +0800

    日志traceid

--- src/test/java/com/haotu/service/HelloServiceTest.java
+++ src/test/java/com/haotu/service/HelloServiceTest.java
@@ -4,8 +4,10 @@ import com.haotu.BaseTestNg;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 public class HelloServiceTest extends BaseTestNg {
 
@@ -16,6 +18,7 @@ public class HelloServiceTest extends BaseTestNg {
 
     @Test
     public void testHello() {
+        MDC.put("traceId", UUID.randomUUID().toString());
         logger.info("{}-{}", "ad", 11);
         logger.error("this is a error");
         logger.warn("this is a warn");
